f1e1c437b2de88a20e867d8a014c219c4a384185
Rachel Roberts <rachel853roberts@gmail.com>
2024-06-22 17:34:43 +0100

    temp push to view previous comments work

--- src/main/java/com/makersacademy/acebook/model/Comment.java
+++ src/main/java/com/makersacademy/acebook/model/Comment.java
@@ -36,3 +36,42 @@ public class Comment {
         this.event = event;
     }
 }
+
+//package com.makersacademy.acebook.model;
+//
+//import lombok.*;
+//        import org.hibernate.annotations.CreationTimestamp;
+//
+//import javax.persistence.*;
+//
+//@Data
+//@Entity
+//@Table(name="COMMENTS")
+//@NoArgsConstructor
+//public class Comment {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Getter
+//    @Setter
+//    private String content;
+//    @ManyToOne
+//    @JoinColumn(name="user_id", nullable = false)
+//    private User user;
+//    @ManyToOne
+//    @JoinColumn(name="event_id", nullable = false)
+//    private Event event;
+//
+//    @Column(nullable = false, updatable = false)
+//    @CreationTimestamp
+//    private java.sql.Timestamp createdAt;
+//
+//    public Comment(String content, User user, Event event, java.sql.Timestamp createdAt) {
+//        this.content = content;
+//        this.createdAt = createdAt;
+//        this.user = user;
+//        this.event = event;
+//    }
+//}
