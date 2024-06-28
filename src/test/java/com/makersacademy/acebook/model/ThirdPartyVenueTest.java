81b8a2c2dbc24e78a06db18ff53a5c90f1d3ac9b
Rachel Roberts <rachel853roberts@gmail.com>
2024-06-28 12:11:21 +0100

    model tests added

---
+++ src/test/java/com/makersacademy/acebook/model/ThirdPartyVenueTest.java
@@ -0,0 +1,11 @@
+package com.makersacademy.acebook.model;
+
+import org.junit.jupiter.api.Test;
+
+public class ThirdPartyVenue {
+    @Test
+    public void testCreateThirdPartyVenue(){
+        ThirdPartyVenue venue = new ThirdPartyVenue();
+        venue.setFullAdress
+    }
+}
